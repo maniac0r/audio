c9c570d58afc9f6ca7324dee9b7754fd277e81a5
maniac0r <maniac@localhost.sk>
2025-03-02 18:40:10 +0100

    more details in prereqs file

--- upcomingvinyl.com/install-prereqs.sh
+++ upcomingvinyl.com/install-prereqs.sh
@@ -1,8 +1,8 @@
 
-# bash script
+
+# for C app version
+sudo apt install fstrcmp libfstrcmp-dev libxml2-dev:arm64
+
+# for bash script version (very slow, obsolete) 
 sudo apt install xml-twig-tools fstrcmp
 
-
-# C app
-sudo apt install fstrcmp libfstrcmp-dev libxml2-dev:arm64
- 
